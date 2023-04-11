33eb8e2aeddb4d7afbd4471cf6be8ec65bdebd0e
jeffureta <jeffvincentureta@gmail.com>
2023-04-11 22:09:58 +0100

    sending json

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function testGS() {
 }
 
 function addGS() {
-    const url = "https://script.google.com/macros/s/AKfycbxjxVx0vR_C-4jQN8Md1LUK5x-RvxRpNNEf5pD_U4SaS1NhYBIJEiY3I4ATTPSMl2uw/exec";
+    // const url = "https://script.google.com/macros/s/AKfycbxjxVx0vR_C-4jQN8Md1LUK5x-RvxRpNNEf5pD_U4SaS1NhYBIJEiY3I4ATTPSMl2uw/exec";
+    const url = "https://script.googleusercontent.com/macros/echo?user_content_key=Ovoz__77-EZSqvGS3k-N4eNO1uNCvaArP81lOQQozto9EDI8IK6i4JW9DvDqaXsrMiPa4awQK64y6AM2aFu3geRBQEs9cm9Wm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnK06Ri5p0hFjzmMRgTYXfbek4juMa2TxRv9Z7Da0rg5hsNn3zvNaE4rBNyNK-wxz5_mtyGfyyGuI0GkHWi-P4qddstkZhpZz1g&lib=MybkFY4sr0YFF2hSIckwlIRmXCs-KDST4"
 
     fetch(url, {
         method: "POST", // *GET, POST, PUT, DELETE, etc. ***REQUIRED
